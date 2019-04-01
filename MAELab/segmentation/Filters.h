04582a89b9b291aa9094b6c6755da50356a787cd
Le Van Linh <linhlevandlu@gmail.com>
2019-04-01 10:52:37 +0200

    add

--- MAELab/segmentation/Filters.h
+++ MAELab/segmentation/Filters.h
@@ -1,8 +1,20 @@
 /*
- * Filters.h
- *
- *  Created on: Apr 7, 2017
- *      Author: linh
+ Morphometry with Automatic Extraction of Landmarks (MAELab)
+ Copyright (C) 2016  Le Van Linh (van-linh.le@u-bordeaux.fr)
+ Created on: Sep 15, 2016
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 
 #ifndef FILTERS_H_
